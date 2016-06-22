7abfa97256f9f4958d71785003002411680344d4
Manuel M T Chakravarty <chak@justtesting.org>
2016-06-22 17:29:19 +1000

    Initial project
    Created a tab bar project and renamed first view to ”Summary”

---
+++ Goals/Goals/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Goals
+//
+//  Created by Manuel M T Chakravarty on 22/06/2016.
+//  Copyright © 2016 Chakravarty & Keller. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Goals/GoalsTests/GoalsTests.swift
@@ -0,0 +1,36 @@
+//
+//  GoalsTests.swift
+//  GoalsTests
+//
+//  Created by Manuel M T Chakravarty on 22/06/2016.
+//  Copyright © 2016 Chakravarty & Keller. All rights reserved.
+//
+
+import XCTest
+@testable import Goals
+
+class GoalsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
